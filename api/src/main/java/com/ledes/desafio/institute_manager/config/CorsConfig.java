e437265e6f0bae2dcda8a0fef8d4313d0ae278c4
Pedro Carvalhaes <pedrohwc1998@gmail.com>
2024-08-12 01:45:52 -0400

    Configurações de permissão de requisições
    Configuração na api para permitir requisições do frontend

---
+++ api/src/main/java/com/ledes/desafio/institute_manager/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.ledes.desafio.institute_manager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*"); // Permite todas as origens
+        config.addAllowedHeader("*"); // Permite todos os cabeçalhos
+        config.addAllowedMethod("*"); // Permite todos os métodos
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
